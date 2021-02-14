6fae679aaf32faebc1c8af5c6b67197be22efbc7
davien21 <chidiebereekennia@gmail.com>
2021-02-14 18:28:01 +0100

    update admin portal

--- assets/js/endpoints.js
+++ assets/js/endpoints.js
@@ -1,4 +1,7 @@
 let local = true;
+console.log(window.location.href)
+
+if ((window.location.href).contains("https")) local = false
 let baseUrl = local
   ? "http://localhost:5000/api/"
   : "https://syntek-api.herokuapp.com/api/";
